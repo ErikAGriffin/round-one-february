f7f86179c14814d28d92ee14251da69e32e5aad3
Erik Griffin <griffin.erik@gmail.com>
2015-02-06 14:04:53 +0000

    first

--- lib/first-round.rb
+++ lib/first-round.rb
@@ -1,2 +1,23 @@
 # Write your code here.
 
+class Wave
+
+
+  def input(message)
+
+    str_array = message.chars
+
+    str_array.map do |x|
+
+      if (str_array.index(x) % 2 == 1)
+        x.upcase! ? x.upcase : x
+      else
+        x.downcase! ? x.downcase : x
+      end
+    end
+
+    str_array.join
+  end
+
+end
+
--- spec/first-round_spec.rb
+++ spec/first-round_spec.rb
@@ -1,8 +1,18 @@
 require('./lib/first-round')
 
-describe('') do
+describe('Wave') do
 
-  it('') do
-    expect().to eq()
+  let (:wave) {Wave.new}
+
+  it('take a string and alternate case') do
+
+    test_string = 'wave'
+    test_result = 'wAvE'
+
+    expect(wave.input(test_string)).to eq test_result
+
+
   end
+
+
 end
