2329804a56ab4e8820fe4a660a6c987b7aa10ffd
Erik Griffin <griffin.erik@gmail.com>
2015-02-06 14:05:18 +0000

    It works

--- spec/first-round_spec.rb
+++ spec/first-round_spec.rb
@@ -6,7 +6,7 @@ describe('Wave') do
 
   it('take a string and alternate case') do
 
-    test_string = 'wave'
+    test_string = 'Wave'
     test_result = 'wAvE'
 
     expect(wave.input(test_string)).to eq test_result
